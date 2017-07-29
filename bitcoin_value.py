6378b1fac2472220b1968ed48ee4e12fb1d7650f
smithgj <smithgj66@hotmail.com>
2017-07-29 11:49:31 -0500

    work stop

--- bitcoin_value.py
+++ bitcoin_value.py
@@ -18,9 +18,21 @@ def read_file():
     input_data = []
     return(input_data)
 
+def wait(msec):
+    pass
+
 
 def go():
-    pass
+    data_list = read_file()
+    while (true):
+        count = 0
+        for i in range(0, len(data_list)):
+            #TODO get quote
+            count = count + 1
+            if ((count % 5) == 0):
+                wait(1000)
+        #TODO write data to csv files
+        wait(1000)
 
 if __name__ == "__main__":
     go()
